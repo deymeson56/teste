5df955176cd0a60732c733c300dfc41c11ffafb3
deymeson56 <76841432+deymeson56@users.noreply.github.com>
2021-03-08 00:10:59 -0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -19,11 +19,7 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDos Attack")
-print
-print "YouTube : https://www.youtube.com/c/HA-MRX"
-print "github : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
-print
+print ""
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
